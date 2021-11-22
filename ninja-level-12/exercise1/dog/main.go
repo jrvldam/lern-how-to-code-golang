db5f8ec32315cf8c0c40bab621f51588b537eebd
jrvldam <jrvldam@gmail.com>
2021-11-22 23:39:41 +0100

    ninja level 12

---
+++ ninja-level-12/exercise1/dog/main.go
@@ -0,0 +1,6 @@
+package dog
+
+// Years takes a human years and return the corresponding dog years
+func Years(humanYears int) int {
+	return humanYears * 7
+}
---
+++ ninja-level-12/exercise1/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jrvldam/lern-how-to-code-golang/ninja-level-12/exercise1/dog"
+)
+
+func main() {
+	fmt.Println(dog.Years(12))
+}
